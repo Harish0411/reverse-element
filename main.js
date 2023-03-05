b9b01f0c89653826ec297065f0fe3217c758d036
Harish0411 <hariharanramasamy98@gmail.com>
2023-03-05 20:54:33 +0530

    reverse the element in array

---
+++ main.js
@@ -0,0 +1,10 @@
+// let array = [3,5,9,12,4,19];
+
+// for(i=0,j=array.length-1; i<j; i++,j--){
+//     temp = array[j];
+//     array[j] = array[i];
+//     array[i] = temp;
+// }
+// console.log(array);
+
+console.log('hello')
